f8e52772e41fd7db818528ff5d7aed6ac6d3737e
Aleksandr Bembel <aleksandr.bembel@gmail.com>
2017-05-20 15:35:38 +0700

    1

--- main.go
+++ main.go
@@ -13,8 +13,38 @@ import (
 // - curl http://localhost:8000/test-some-path
 func main() {
 	r := router.New()
-	http.HandleFunc("/", handler)
-	http.ListenAndServe("localhost:8000", nil)
+
+	a := 5
+
+	r.POST("/api/v1/users", mw(a, abc))
+
+	r.Listen(":8000")
+}
+
+func mw(a int, h router.Handle) router.Handle {
+	// ? как передать a в h?
+	return h
+}
+
+func pampam(c *router.Control) {
+	fmt.Fprintf(
+		c.Writer, "URL.Path = %q\n", c.Request.URL.Path,
+	)
+
+}
+
+func abc(c *router.Control) {
+	fmt.Fprintf(
+		c.Writer, "a = ?",
+	)
+
+}
+
+func home(c *router.Control) {
+	fmt.Fprintf(
+		c.Writer, "URL.Path = %q\n", c.Request.URL.Path,
+	)
+
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
